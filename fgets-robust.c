e32a02246abaa92707300f378567c945b9ee59f2
Awwal <awwalonpc@gmail.com>
2025-09-11 11:48:42 +0100

    Modified program to handle empty input (user presses enter without typing anything), and also removed the double getchar in the while loop to stop getchar from happening twice in one condition

--- fgets-robust.c
+++ fgets-robust.c
@@ -10,15 +10,22 @@ int main() {
 	for (int i = 0; i < sizeof(sentence); i++) {
 		if (sentence[i] == '\n') {
 			sentence[i] = '\0';
-			printf("Valid input(less than max). You entered: %s", sentence);
+			if (i == 0) {
+				printf("Empty input. \n");
+				}
+			else {
+				printf("Valid input(less than max). You entered: %s", sentence);
+				}
 			break;
 			}
+			
 		else if (sentence[i] == '\0') {
 			if (getchar() == '\n') {
 				printf("Valid input(exactly max). You entered: %s", sentence);
 				}
 			else {
-				while (getchar() != '\n' && getchar() != EOF) {
+				int c;
+				while ((c = getchar()) != '\n' && c != EOF) {
 					}
 				printf("Invalid input(longer than max). Truncation occured. You entered: %s", sentence);
 				}	
